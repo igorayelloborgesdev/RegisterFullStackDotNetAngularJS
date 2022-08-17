dd0092e8744c2550a6c041088adc5ae5af661784
Igor <igorayelloborgesdev@gmail.com>
2022-08-17 02:24:21 -0300

    Teste unitário

---
+++ API/APIFuncionario/APIFuncionarioTeste/Service/SetorServiceTeste.cs
@@ -0,0 +1,32 @@
+﻿using APIFuncionario.IService;
+using APIFuncionario.Models;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APIFuncionarioTeste.Service
+{
+    public class SetorServiceTeste
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void ConsultarSetores_RetornaResultado_IsTrue()
+        {
+            // arrange
+            Mock<ISetorService> mock = new Mock<ISetorService>();
+            IEnumerable<Setor> setorList = new List<Setor>();
+            mock.Setup(m => m.ConsultarSetores()).Returns((Task<APIFuncionario.Response.ResponseObject<Setor>>)setorList);
+            // act
+            // assert
+            Assert.IsTrue(true);
+        }
+    }
+}
---
+++ API/APIFuncionario/APIFuncionarioTeste/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace APIFuncionarioTeste
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
